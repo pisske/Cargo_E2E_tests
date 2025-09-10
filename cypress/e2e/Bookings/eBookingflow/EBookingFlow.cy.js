7362b094c7e55d46d818767c25cebb6b9545ef96
Isidora <isidora.petrovic@wedoqa.co>
2025-09-10 13:40:02 +0200

    continue test development on ebooking flow

--- cypress/e2e/Bookings/eBookingflow/EBookingFlow.cy.js
+++ cypress/e2e/Bookings/eBookingflow/EBookingFlow.cy.js
@@ -8,14 +8,8 @@ describe("Forwarder Booking Flow UI", () => {
   it("Performs eBooking flow - total icon and confirm click", () => {
     cy.visit("/forwarder/search/forwarder-search");
 
-    PageEBookingFlow.changeLoadType();
+    // PageEBookingFlow.changeLoadType();
   });
-  // it("should have SIN - Singapore Changi auto-filled in origin", () => {
-  //   PageEBookingFlow.getOriginValue().should(
-  //     "have.value",
-  //     "SIN - Singapore Changi"
-  //   );
-  // });
 
   it("selects the second destination option after typing cdg", () => {
     PageEBookingFlow.typeDestination("cdg");
--- pageObjects/Bookings/eBookingFlow/PageEBookingFlow.js
+++ pageObjects/Bookings/eBookingFlow/PageEBookingFlow.js
@@ -7,19 +7,29 @@ class ForwarderBookingPage {
   changeLoadType() {
     cy.get(".icon-total").click();
   }
-  // getOriginValue() {
-  //   return cy
-  //     .get('input[formcontrolname="origin"]', { timeout: 10000 })
-  //     .should("be.visible");
-  // }
+
   typeDestination(destination) {
+    const expectedOrigin = "SIN - Singapore Changi";
+
+    cy.get('input[placeholder="Origin"]', { timeout: 10000 })
+      .should("be.visible")
+      .should("have.value", expectedOrigin);
+
     cy.get('input[placeholder="Destination"]')
+      .should("be.visible")
       .clear()
-      .type(destination)
-      .wait(500)
-      .type("{downarrow}")
-      .type("{enter}");
+      .type(destination, { delay: 150 })
+      .then(($input) => {
+        $input[0].dispatchEvent(new Event("input", { bubbles: true }));
+      });
+    cy.pause();
+    cy.wait(1500);
+
+    cy.get('input[placeholder="Destination"]').type(
+      "{downarrow}{downarrow}{enter}"
+    );
+
+    cy.get('input[placeholder="Destination"]').type("{downarrow}{enter}");
   }
 }
-
 export default new ForwarderBookingPage();
